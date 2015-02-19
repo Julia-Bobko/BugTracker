9f5292e83f9915b05a1a26fd6855ce3f90df0480
Julia-Bobko <yulia.bobko@mail.ru>
2015-02-19 16:47:44 +0300

    adeded status and ticketService

---
+++ BugTracker.Web/app/js/services/ticketService.js
@@ -0,0 +1,23 @@
+﻿angular.module('moduleApp').
+factory('ticketService', ['$http', '$q', function ($http, $q) {
+    return {
+        getTickets: getTickets,
+        getTicket: getTicket
+    };
+
+    function getTickets() {
+        var deferred = $q.defer();
+        $http.get('/api/ticket').success(function (data) {
+            deferred.resolve(data);
+        });
+        return deferred.promise;
+    }
+
+    function getTicket(ticketid) {
+        var deferred = $q.defer();
+        $http.get('/api/ticket/' + ticketid).success(function (data) {
+            deferred.resolve(data);
+        });
+        return deferred.promise;
+    }
+}]);
