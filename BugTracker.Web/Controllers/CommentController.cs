3b377123e0a7d14f059c5414dcbfd4f0d33bd0c5
Julia-Bobko <yulia.bobko@mail.ru>
2015-02-25 14:09:19 +0300

    added CommentController.cs

---
+++ BugTracker.Web/Controllers/CommentController.cs
@@ -0,0 +1,29 @@
+﻿using BugTracker.Entities;
+using BugTracker.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BugTracker.Web.Controllers
+{
+    public class CommentController : ApiController
+    {
+        CommentRepository repo;
+
+        CommentController() {
+            repo = new CommentRepository();
+        }
+        public void PostComment(Comment comment)
+        {
+            repo.AddComment(comment);
+        }
+
+        public List<Comment> GetComments()
+        {
+            return repo.GetComments();
+        }
+    }
+}
