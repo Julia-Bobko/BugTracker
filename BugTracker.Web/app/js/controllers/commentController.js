1205c4702bd8a4d46c414d11b258f7082ac9924f
Julia-Bobko <yulia.bobko@mail.ru>
2015-02-25 14:16:21 +0300

    created commentController and Servise.js

---
+++ BugTracker.Web/app/js/controllers/commentController.js
@@ -0,0 +1,7 @@
+﻿controllersApp.controller('CommentCtrl', ['$scope', '$stateParams', 'commentService', 'ticketService', function ($scope, $stateParams, commentService, ticketService) {
+    $scope.addComment = function () {
+        commentService.addComment($scope.comment, $stateParams.ticketId).then(function (value) {
+            $scope.comment = "";          
+        });
+    }
+}]);
---
+++ BugTracker.Web/app/js/services/commentService.js
@@ -0,0 +1,18 @@
+﻿angular.module('moduleApp').
+factory('commentService', ['$http', '$q', function ($http, $q) {
+    return {
+        addComment: addComment
+    };
+
+    function addComment(comment, ticketId) {
+        var deferred = $q.defer();
+        var comments = {
+            IdTicket: ticketId,
+            Comments: comment
+        };
+        $http.post('/api/comment', comments).success(function (data) {
+            deferred.resolve(data);
+        });
+        return deferred.promise;
+    }
+}]);
