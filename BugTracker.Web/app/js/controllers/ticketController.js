054f33d4aaae67d81712daea2892aef1664906ea
Julia-Bobko <yulia.bobko@mail.ru>
2015-02-25 14:17:11 +0300

    changet ticketController.js

--- BugTracker.Web/app/js/controllers/ticketController.js
+++ BugTracker.Web/app/js/controllers/ticketController.js
@@ -2,5 +2,8 @@
     function ($scope, ticketService, $stateParams) {
     ticketService.getTickets().then(function (value) { $scope.tickets = value; });
     ticketService.getTicket($stateParams.ticketId).then(function (value) { $scope.ticket = value; });
+    $scope.hideComment = function () { $scope.isComment = false; }
+    $scope.showComment = function () { $scope.isComment = true; }
+    $scope.url = "/" + $stateParams.ticket + "/" + $stateParams.ticketId + "#textComment";
 }]);
 
