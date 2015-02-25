45e20af76196e17289dfc346b6f3c8a6aa25494c
Julia-Bobko <yulia.bobko@mail.ru>
2015-02-25 14:14:57 +0300

    changet app.js and main.css

--- BugTracker.Web/app/js/app.js
+++ BugTracker.Web/app/js/app.js
@@ -1,27 +1,31 @@
 ﻿var moduleApp = angular.module('moduleApp', ['ui.router', 'controllersApp', 'dragDrop']);
 moduleApp.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/tickets');
+    $urlRouterProvider.otherwise('menu');
     $stateProvider
-        .state('tickets', {
+        .state('menu.tickets', {
             url: '/tickets',
             templateUrl: 'app/partials/ticketsList.html',
             controller: 'TicketCtrl'
+        })
+        .state('menu', {
+            url: '/menu',
+            templateUrl: 'app/partials/menu.html'            
         })
         .state('ticket', {
             url: '/tickets/:ticketId',
             templateUrl: 'app/partials/ticketDetail.html',
             controller: 'TicketCtrl'
         })
-        .state('path4', {
+        .state('menu.path4', {
             url: '/path4'
         })
-        .state('path1', {
+        .state('menu.path1', {
         url: '/path1'
         })
-        .state('path2', {
+        .state('menu.path2', {
             url: '/path2'
         })
-        .state('path3', {
+        .state('menu.path3', {
             url: '/path3'
         });
 });
